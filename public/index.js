7913b0058d2216046cab6a8a1310accc5c2b0677
Malsgr <tomaspmdc@gmail.com>
2026-03-23 08:29:50 +0000

    empezando index.html

---
+++ public/index.js
@@ -0,0 +1,14 @@
+
+
+async function fetchData(url) {
+    const response = await fetch(url);
+    const data = data;
+    return data;
+}
+
+const response = await fetchData();
+
+if (response){
+
+
+}
